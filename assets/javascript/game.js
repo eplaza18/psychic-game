de9c50afd28cb2cb1f9b496b7867df8eab73e307
erikplaza@gmail.com <erikplaza@gmail.com>
2019-02-06 20:10:16 -0800

    Got some of JS working correctly.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,50 @@
 var computerChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
 
 var wins = 0;
-var losses = 0;
+var loss = 0;
+var guesses = 10;
+
+
+
 var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var lettersGuessed = document.getElementById("letters-guessed");
+var lossText = document.getElementById('losses-text');
+var lettersGuessed = document.getElementById("letters-guessed")
+var guessesLeft = document.getElementById("guesses-left");
+
+//Computer guesses random character from array.
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+console.log(computerGuess);
 
 document.onkeyup = function(event) {
     // Determines which key was pressed.
     var userGuess = event.key;
-    //Computer guesses random character from array.
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
+    
     //If win, tally one win and reset game
     if (userGuess === computerGuess) {
-        console.log("You win!"); 
+        wins++;
+        alert ("You Win!");       
     }
+
     //Write letter guessed, and take one guess away, continue game.
-    else (userGuess !== computerGuess) 
-    //Write letter guessed
-    console.log(userGuess);
-    // document.write(lettersguessed=userGuess);
-    
+    else if (userGuess !== computerGuess) {
+
+        var newDiv = document.createElement("div");
+        lettersGuessed.appendChild(newDiv);
+        newDiv.textContent = userGuess + ", ";
+
+        for (var i = 10; i > 0; i--) {
+            guessesLeft.textContent = guesses - 1;            
+        }
+        
+        if (guesses === 1) {
+            loss++;
+            alert ("You Lose, try again.")
+            // location.reload();
+        }
+        guesses--;
+       }
+       winsText.textContent = 'Wins: ' + wins;
+       lossText.textContent = 'Losses: ' + loss;
+}     
     //Take 1 guess away, repeat until none left, write loss, restart
 
-    
-    }
-
-    
